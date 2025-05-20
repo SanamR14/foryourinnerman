577b7253461ac8eea9552b17b8b4a25c70d9b261
SanamR14 <sanamrajaboopathi@gmail.com>
2025-05-20 19:36:09 +0100

    jwt changes

--- api/utils/jwt.ts
+++ api/utils/jwt.ts
@@ -1,6 +1,6 @@
 import jwt from 'jsonwebtoken';
 
-const JWT_SECRET = process.env['JWT_SECRET'] || 'yoursecret';
+const JWT_SECRET = process.env.JWT_SECRET || 'yoursecret';
 
 export function signToken(payload: object) {
   return jwt.sign(payload, JWT_SECRET, { expiresIn: '1d' });
